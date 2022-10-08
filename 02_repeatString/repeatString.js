205a927e66e1ade2d89b51d6e9172b0663f0ea7f
JamesSharley <jssharley@hotmail.com>
2022-10-08 15:12:08 +0100

    sat 8th oct

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function (x, y) {
 
+
+    if (y < 0) {
+        return 'ERROR'
+    }
+    else {
+        const stringR = x.repeat(y);
+        return stringR;
+    }
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function (x) {
+    let split = x.split("");
+    let revSplit = split.reverse();
+    let revSplitJoin = revSplit.join("");
 
+    return revSplitJoin;
 };
 
 // Do not edit below this line
